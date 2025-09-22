c90d12baf2cd932c87d4329d68d94ba2d30cfdb5
Vespid90 <farid.elmam@gmail.com>
2025-09-22 11:03:32 +0200

    cancel on stock picking done

--- bestcar_commercial/models/stock_picking.py
+++ bestcar_commercial/models/stock_picking.py
@@ -12,3 +12,14 @@ class Project(models.Model):
                 else:
                     rec.product_id.product_tmpl_id.status = "delivered"
         return res
+
+
+    def action_cancel(self):
+        res = super().action_cancel()
+        for rec in self:
+            if rec.product_id.product_tmpl_id.is_vehicle:
+                if rec.product_id.product_tmpl_id.status == "waiting_arrival":
+                    rec.product_id.product_tmpl_id.status = "added"
+                else:
+                    rec.product_id.product_tmpl_id.status = "added"
+        return res
