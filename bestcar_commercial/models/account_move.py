a99ab7c64f0b9c2e0b4bb3e6b19fbc93bebe538a
Vespid90 <farid.elmam@gmail.com>
2025-09-22 11:14:38 +0200

    cancel on account move done

--- bestcar_commercial/models/account_move.py
+++ bestcar_commercial/models/account_move.py
@@ -10,3 +10,11 @@ class AccountMove(models.Model):
                 if line.product_id.product_tmpl_id.is_vehicle:
                     line.product_id.product_tmpl_id.status = "payment"
         return res
+
+    def button_cancel(self):
+        res = super().button_cancel()
+        for move in self:
+            for line in move.invoice_line_ids:
+                if line.product_id.product_tmpl_id.is_vehicle:
+                    line.product_id.product_tmpl_id.status = "for_sale"
+        return res
