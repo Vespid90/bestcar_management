46b476b2a246f83eaeb3f04cf342a2d7a7a469e4
Vespid90 <farid.elmam@gmail.com>
2025-10-03 10:04:12 +0200

    translate done

--- bestcar_commercial/models/vehicle_type.py
+++ bestcar_commercial/models/vehicle_type.py
@@ -5,4 +5,4 @@ class VehicleType(models.Model):
     _name = "vehicle.type"
     _description = "Vehicle Type"
 
-    name = fields.Char(string="Type")
+    name = fields.Char(string="Type", translate=True)
